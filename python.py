586b2b2242cbb426529f610893461bf4cc4acdc9
С Г <guplik@bk.ru>
2023-02-19 18:51:33 +0300

    Первое сделанно

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
-c = input("Введите c \n")
-y = input("Введите y \n")
-x = input("Введите x \n")
+c = int(input("Введите c \n"))
+y = int(input("Введите y \n"))
+x = int(input("Введите x \n"))
 
-h = ((c*y + x*x*(y**5 - 1)) / c-x-y ) + ((2 * x**3 + 3)/(y -x)**2 )
+from math import fabs
+h = ((c*y + x*x*(y**5 - 1)) / fabs(c)-abs(x)-abs(y) ) + ((2 * x**3 + 3)/(y -x)**2)
 print(h) 
