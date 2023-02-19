d2b052d7935ca29033db5f23f992c36f9cc3c7c0
С Г <guplik@bk.ru>
2023-02-19 18:50:27 +0300

    1 первое сделанно

--- python.py
+++ python.py
@@ -2,5 +2,5 @@ c = input("Введите c \n")
 y = input("Введите y \n")
 x = input("Введите x \n")
 
-h = (c*y + x*x*(y**5 - 1)) / c-x-y
+h = ((c*y + x*x*(y**5 - 1)) / c-x-y ) + ((2 * x**3 + 3)/(y -x)**2 )
 print(h) 
