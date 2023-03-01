98db3fb16aab1114d3fb35fe32c5817e10ca0474
tiktakg <guplik@bk.ru>
2023-03-01 14:53:18 +0300

    Добавлены 6 задачи

--- 5.2Variant2.py
+++ 5.2Variant2.py
@@ -1,33 +1,6 @@
-file = open('fileInput5.2.txt')
-str = file.read()
+i = int(input("Введите k \n"))
+numbers = [1, 2, 3, 4, 5]
 
-isNumber,isUpper,isLower = False,False,False
-
-if(len(str) < 20):
-    if any(char.isdigit() for char in str):
-            isNumber = True
-    if any(char.isupper() for char in str):
-            isUpper = True
-    if any(char.lower() for char in str):
-            isLower = True
-    if(isLower and isUpper and isNumber):
-        print("Файле подходит")
-else:
-    print("Файле меньше 20 символов")
-
-        
-
-
-strWithDouble = ""
-for char in str:
-    if(char.isalpha()):
-        strWithDouble +=char + char
-    else:
-        strWithDouble +=char
-
-fileOutput = open("fileOutput5.2.txt","w", encoding='utf-8')
-fileOutput.write(strWithDouble)
-fileOutput.close()
 
 
 
