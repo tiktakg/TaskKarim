d2040cbf3009c194aba38964a57607a679dbd83b
С Г <guplik@bk.ru>
2023-03-02 17:36:06 +0300

    комит

--- 4.5Variant8.py
+++ 4.5Variant8.py
@@ -1,3 +1,25 @@
+
+while 1:
+    x = int(input("Введите x \n"))
+    x1 = int(input("Введите x1 \n"))
+    y = int(input("Введите y \n"))
+    y1 = int(input("Введите y1 \n"))
+    print((x*x1)-y*y1, "Отвте")
+
+    a11 = int(input("Введите x \n "))
+    a22 = int(input( "Введите x1 \n"))
+    a33 = int(input("Введите y \n"))
+    a12 = int(input("Введите y1 \n"))
+    a23  = int(input("Введите x \n"))
+    a31 = int(input("Введите x1 \n"))
+    a13 = int(input( "Введите y \n"))
+    a21 = int(input("Введите y1 \n"))
+    a32 = int(input("Введите y1 \n"))
+
+    print((a11*a22*a33)+ (a12*a23*a31)+(a13*a21*a32)-(a13*a22*a31)-(a11*a23*a31)-(a11*a23*a32)-(a12*a21*a33))
+
+
+
 # x = float(input("Введите x \n"))
 
 # a,n= -1,0
