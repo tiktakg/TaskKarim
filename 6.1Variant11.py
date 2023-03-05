2978df41d1755e0ef6cdc238aa3eca10321fcb6f
С Г <guplik@bk.ru>
2023-03-05 12:59:57 +0300

    Сделанно 6.2

--- 6.1Variant11.py
+++ 6.1Variant11.py
@@ -1,29 +1,29 @@
-B = input("Введите массив b состоящий из 8 элементов \n").split()
-B = [float (x) for x in B]
+B = []
+for i in range(8):
+    number = float(input("Введите число \n"))
+    B.append(number)
+
 x = float(input("Введите x \n"))
 
 sum,positiveElement = 0.0,0
 
 import math   
-if(len(B) == 8):
-    for i in range(len(B)):
-        B[i] = (math.cos(B[i]**2) / math.sin(B[i]**2) ) - x* math.sqrt(2*B[i])
-    
-        if(B[i] > 0):
-            positiveElement +=1
 
-    for i in range(2,len(B),2):
-        sum += B[i]
-    
+for i in range(len(B)):
+    B[i] = (math.cos(B[i]**2) / math.sin(B[i]**2) ) - x* math.sqrt(2*B[i])
 
-    fileOutput = open("filesOutput/fileOutput6.1.txt","w", encoding='utf-8')
+    if(B[i] > 0):
+        positiveElement +=1
 
-    fileOutput.write("Полученный массив" + str(B)+ "\n")
-    fileOutput.write("Cумма элементов с четным индексом " + str(sum) + "\n")
-    fileOutput.write("Количество положительных элементов " + str(positiveElement)+ "\n")
+for i in range(2,len(B),2):
+    sum += B[i]
 
-    fileOutput.close()
- 
-else:
-    print("Элеметов меньше 8")
-    
+
+fileOutput = open("filesOutput/fileOutput6.1.txt","w", encoding='utf-8')
+
+fileOutput.write("Полученный массив" + str(B)+ "\n")
+fileOutput.write("Cумма элементов с четным индексом " + str(sum) + "\n")
+fileOutput.write("Количество положительных элементов " + str(positiveElement)+ "\n")
+
+fileOutput.close()
+
--- 6.2Variant1.py
+++ 6.2Variant1.py
@@ -1,1 +1,29 @@
-import array 
+import math
+import array as a
+
+A = a.array('f')
+b = float(input("Введите b \n"))
+minElement, element, positionMinElement = 0, 0, 0
+
+for i in range(10):
+    A.append(float(input("Введите число \n")))
+    A[i] = 3*math.sqrt(b*A[i]) - math.tan(2) * A[i]
+
+    if (i == 0):
+        minElement = A[0]
+    element = A[i]
+
+    if (minElement >= A[i]):
+        minElement = element
+        positionMinElement = i
+
+
+fileOutput = open("filesOutput/fileOutput6.2.txt", "w", encoding='utf-8')
+fileOutput.write("Минимальный элемент \n" + str(minElement) + "\n")
+fileOutput.write("Полученный массив \n" + str(' '.join(map(str, A))) + "\n")
+
+A[positionMinElement] = A[9]
+A[9] = minElement
+
+fileOutput.write("Минимальный и последний элемент поменяны местами массив \n" + str(' '.join(map(str, A))) + "\n")
+fileOutput.close()
