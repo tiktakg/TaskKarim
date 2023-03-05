2ec576f3d367ac9aa807877991da56d2088a9be5
С Г <guplik@bk.ru>
2023-03-05 13:05:21 +0300

    )

--- 6.2Variant1.py
+++ 6.2Variant1.py
@@ -3,27 +3,24 @@ import array as a
 
 A = a.array('f')
 b = float(input("Введите b \n"))
-minElement, element, positionMinElement = 0, 0, 0
+positionMinElement = 0
 
 for i in range(10):
     A.append(float(input("Введите число \n")))
     A[i] = 3*math.sqrt(b*A[i]) - math.tan(2) * A[i]
 
     if (i == 0):
-        minElement = A[0]
-    element = A[i]
+        positionMinElement = 0
 
-    if (minElement >= A[i]):
-        minElement = element
+    if (A[positionMinElement] >= A[i]):
         positionMinElement = i
 
 
 fileOutput = open("filesOutput/fileOutput6.2.txt", "w", encoding='utf-8')
-fileOutput.write("Минимальный элемент \n" + str(minElement) + "\n")
+fileOutput.write("Минимальный элемент \n" + str(A[positionMinElement]) + "\n")
 fileOutput.write("Полученный массив \n" + str(' '.join(map(str, A))) + "\n")
 
-A[positionMinElement] = A[9]
-A[9] = minElement
+A[positionMinElement], A[9]  = A[9],A[positionMinElement]
 
 fileOutput.write("Минимальный и последний элемент поменяны местами массив \n" + str(' '.join(map(str, A))) + "\n")
 fileOutput.close()
