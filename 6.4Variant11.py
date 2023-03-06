3fbd3b82c3c08398e4ab6e556d9df8b1bb4151c0
tiktakg <guplik@bk.ru>
2023-03-06 10:08:14 +0300

    6.4 сделанно

--- 6.4Variant11.py
+++ 6.4Variant11.py
@@ -7,7 +7,9 @@ for i in range(n):
     matrix.append([])
     for j in range(m):
         matrix[i].append(((math.cos(2) / math.sin(2)) * i)- math.sqrt(3 * j*2))
-        negativeElement *= matrix[i][j]
+        if(matrix[i][j] < 0 ):
+            negativeElement *= matrix[i][j]
+
         if(j == 2):
             sumSecondColumn += float(matrix[i][j])
         
@@ -19,9 +21,14 @@ for i in range(n):
        
         
 
-print(matrix[3][minElement])
-print(sumSecondColumn)
+fileOutput = open("filesOutput/fileOutput6.4.txt", "w", encoding='utf-8')
 
+fileOutput.write("Сумма элементов второго столбца" + str(sumSecondColumn) + "\n")
+fileOutput.write("Минимальный элемент 3 строки " + str(matrix[3][minElement]) + "\n")
+fileOutput.write("Произведение отрицательных элементов матрицы B " + str(negativeElement) + "\n")
+fileOutput.write("Вся матрица \n ")
 for i in range(n):
-    print(matrix[i])
+    fileOutput.write(str(matrix[i]) + "\n")
+    
+fileOutput.close()
 
