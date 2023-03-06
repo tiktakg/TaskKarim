43bfb083a8854fb9e0609d3356f9d6790badcab7
С Г <guplik@bk.ru>
2023-03-06 07:00:33 +0300

    Сделанно 6.4

--- 6.4Variant11.py
+++ 6.4Variant11.py
@@ -1,21 +1,25 @@
 n = int(input("Введите n размера матрицы :\n"))
 m = int(input("Введите m размера матрицы :\n"))
-matrix,minElement,sumSecondColumn = [],0,0
+matrix,minElement,sumSecondColumn,negativeElement = [],0,0,0
 
 import math 
 for i in range(n):
     matrix.append([])
     for j in range(m):
         matrix[i].append(((math.cos(2) / math.sin(2)) * i)- math.sqrt(3 * j*2))
-    
-    if(i == 1):
-        print(matrix[1][i])
-        sumSecondColumn += matrix[1][i]
+        negativeElement *= matrix[i][j]
+        if(j == 2):
+            sumSecondColumn += float(matrix[i][j])
+        
+        if(i == 3):
+            if(j == 0):
+                minElement = j
+            elif(matrix[i][minElement] > matrix[i][j]):
+                minElement = j
        
         
 
-print(matrix[1][1])
-
+print(matrix[3][minElement])
 print(sumSecondColumn)
 
 for i in range(n):
